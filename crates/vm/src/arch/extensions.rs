794ddcef5a9f6f8dc4de0c71002c10fac73949eb
schaeff <thibaut@schaeff.fr>
2025-03-25 17:06:02 +0100

    wip

--- crates/vm/src/arch/extensions.rs
+++ crates/vm/src/arch/extensions.rs
@@ -1065,13 +1065,13 @@ impl<F: PrimeField32, E, P> VmChipComplex<F, E, P> {
             let air_proof_input = match chip_id {
                 ChipId::Executor(id) => {
                     let chip = self.inventory.executors.pop().unwrap();
-                    println!("generate air proof input for executor {}", chip.air_name());
+                    println!("generate air proof input for executor {} of length {}", chip.air_name(), chip.current_trace_height());
                     assert_eq!(id, self.inventory.executors.len());
                     generate_air_proof_input(chip, height)
                 }
                 ChipId::Periphery(id) => {
                     let chip = self.inventory.periphery.pop().unwrap();
-                    println!("generate air proof input for periphery {}", chip.air_name());
+                    println!("generate air proof input for periphery {} of length {}", chip.air_name(), chip.current_trace_height());
                     assert_eq!(id, self.inventory.periphery.len());
                     generate_air_proof_input(chip, height)
                 }
