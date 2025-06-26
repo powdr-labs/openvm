powdr-labs/openvm

commit bb20ff896b4b37ee47ca95fcb7aff8b4015553b6

refactor(new-execution): make `ExecutionCtrl`s stateless (#1789)
- move all state from `ExecutionCtrl`s to `Ctx`
- add a `constant_trace_heights` function to `VmChipComplex`
- split `MeteredCtx` into `MemoryCtx` and `SegmentationCtx`

crates/vm/src/arch/vm.rs
@@ -28,9 +28,9 @@ use thiserror::Error;
 use tracing::info_span;
 
 use super::{
-    ChipId, ExecutionError, InsExecutorE1, MemoryConfig, VmChipComplex, VmComplexTraceHeights,
-    VmConfig, VmInventoryError, CONNECTOR_AIR_ID, MERKLE_AIR_ID, PROGRAM_AIR_ID,
-    PROGRAM_CACHED_TRACE_INDEX, PUBLIC_VALUES_AIR_ID,
+    execution_mode::e1::E1Ctx, ChipId, ExecutionError, InsExecutorE1, MemoryConfig, VmChipComplex,
+    VmComplexTraceHeights, VmConfig, VmInventoryError, CONNECTOR_AIR_ID, MERKLE_AIR_ID,
+    PROGRAM_AIR_ID, PROGRAM_CACHED_TRACE_INDEX, PUBLIC_VALUES_AIR_ID,
 };
 #[cfg(feature = "bench-metrics")]
 use crate::metrics::VmMetrics;
@@ -38,8 +38,8 @@ use crate::{
     arch::{
         execution_mode::{
             e1::E1ExecutionControl,
-            metered::{ctx::Segment, MeteredCtx, MeteredExecutionControl},
-            tracegen::TracegenExecutionControl,
+            metered::{MeteredCtx, MeteredExecutionControl, Segment},
+            tracegen::{TracegenCtx, TracegenExecutionControl},
         },
         hasher::poseidon2::vm_poseidon2_hasher,
         VmSegmentExecutor, VmSegmentState,
@@ -228,20 +228,21 @@ where
             chip_complex,
             self.trace_height_constraints.clone(),
             exe.fn_bounds.clone(),
-            E1ExecutionControl::new(instret_end),
+            E1ExecutionControl,
         );
         #[cfg(feature = "bench-metrics")]
         {
             segment.metrics = state.metrics;
         }
 
+        let ctx = E1Ctx::new(instret_end);
         let mut exec_state = VmSegmentState::new(
             state.instret,
             state.pc,
             Some(GuestMemory::new(state.memory)),
             state.input,
             state.rng,
-            (),
+            ctx,
         );
         segment.execute_spanned("execute_e1", &mut exec_state)?;
 
@@ -290,20 +291,11 @@ where
             create_and_initialize_chip_complex(&self.config, exe.program.clone(), None, None)
                 .unwrap();
         let air_names = chip_complex.air_names();
-        // TODO(ayush): get rid of segmentation_strategy altogether
-        let ctrl = MeteredExecutionControl::new(&air_names, widths, interactions)
-            .with_max_trace_height(
-                self.config
-                    .system()
-                    .segmentation_strategy
-                    .max_trace_height() as u32,
-            )
-            .with_max_cells(self.config.system().segmentation_strategy.max_cells());
         let mut executor = VmSegmentExecutor::<F, VC, _>::new(
             chip_complex,
             self.trace_height_constraints.clone(),
             exe.fn_bounds.clone(),
-            ctrl,
+            MeteredExecutionControl,
         );
 
         #[cfg(feature = "bench-metrics")]
@@ -315,23 +307,40 @@ where
             .chip_complex
             .memory_controller()
             .continuation_enabled();
+        let as_alignment = executor
+            .chip_complex
+            .memory_controller()
+            .memory
+            .min_block_size
+            .iter()
+            .map(|&x| log2_strict_usize(x as usize) as u8)
+            .collect();
+        let constant_trace_heights = executor
+            .chip_complex
+            .constant_trace_heights()
+            .collect::<Vec<_>>();
+
         let ctx = MeteredCtx::new(
-            widths.len(),
+            constant_trace_heights,
             continuations_enabled,
-            executor
-                .chip_complex
-                .memory_controller()
-                .memory
-                .min_block_size
-                .iter()
-                .map(|&x| log2_strict_usize(x as usize) as u8)
-                .collect(),
+            as_alignment,
             executor
                 .chip_complex
                 .memory_controller()
                 .mem_config()
                 .memory_dimensions(),
-        );
+            air_names,
+            widths.to_vec(),
+            interactions.to_vec(),
+        )
+        // TODO(ayush): get rid of segmentation_strategy altogether
+        .with_max_trace_height(
+            self.config
+                .system()
+                .segmentation_strategy
+                .max_trace_height() as u32,
+        )
+        .with_max_cells(self.config.system().segmentation_strategy.max_cells());
 
         let mut exec_state = VmSegmentState::new(
             state.instret,
@@ -345,7 +354,7 @@ where
 
         check_termination(exec_state.exit_code)?;
 
-        Ok(exec_state.ctx.segments)
+        Ok(exec_state.ctx.into_segments())
     }
 
     pub fn execute_metered(
@@ -398,12 +407,11 @@ where
             )
             .unwrap();
 
-            let ctrl = TracegenExecutionControl::new(Some(state.instret + num_insns));
             let mut segment = VmSegmentExecutor::<_, VC, _>::new(
                 chip_complex,
                 self.trace_height_constraints.clone(),
                 exe.fn_bounds.clone(),
-                ctrl,
+                TracegenExecutionControl,
             );
 
             #[cfg(feature = "bench-metrics")]
@@ -411,8 +419,10 @@ where
                 segment.metrics = state.metrics;
             }
 
+            let instret_end = state.instret + num_insns;
+            let ctx = TracegenCtx::new(Some(instret_end));
             let mut exec_state =
-                VmSegmentState::new(state.instret, state.pc, None, state.input, state.rng, ());
+                VmSegmentState::new(state.instret, state.pc, None, state.input, state.rng, ctx);
             segment
                 .execute_spanned("execute_e3", &mut exec_state)
                 .map_err(&map_err)?;
@@ -657,15 +667,14 @@ where
         let chip_complex =
             create_and_initialize_chip_complex(&self.config, exe.program.clone(), None, None)
                 .unwrap();
-        let ctrl = E1ExecutionControl::default();
         let mut executor = VmSegmentExecutor::<F, VC, _>::new(
             chip_complex,
             self.trace_height_constraints.clone(),
             exe.fn_bounds.clone(),
-            ctrl,
+            E1ExecutionControl,
         );
 
-        let ctx = ();
+        let ctx = E1Ctx::default();
 
         let mut exec_state = VmSegmentState::new(
             0,
@@ -696,37 +705,46 @@ where
             create_and_initialize_chip_complex(&self.config, exe.program.clone(), None, None)
                 .unwrap();
         let air_names = chip_complex.air_names();
-        let ctrl = MeteredExecutionControl::new(&air_names, widths, interactions)
-            .with_max_trace_height(
-                self.config
-                    .system()
-                    .segmentation_strategy
-                    .max_trace_height() as u32,
-            )
-            .with_max_cells(self.config.system().segmentation_strategy.max_cells())
-            // TODO(ayush): this is temporary way to prevent segmentation
-            //              add a metered execution mode with no segmentation
-            .with_segment_check_insns(u64::MAX);
         let mut executor = VmSegmentExecutor::<F, VC, _>::new(
             chip_complex,
             self.trace_height_constraints.clone(),
             exe.fn_bounds.clone(),
-            ctrl,
+            MeteredExecutionControl,
         );
 
+        let continuations_enabled = executor
+            .chip_complex
+            .memory_controller()
+            .continuation_enabled();
+        let as_alignment = executor
+            .chip_complex
+            .memory_controller()
+            .memory
+            .min_block_size
+            .iter()
+            .map(|&x| log2_strict_usize(x as usize) as u8)
+            .collect();
+        let constant_trace_heights = executor
+            .chip_complex
+            .constant_trace_heights()
+            .collect::<Vec<_>>();
+
         let ctx = MeteredCtx::new(
-            widths.len(),
-            self.config.system().continuation_enabled,
-            executor
-                .chip_complex
-                .memory_controller()
-                .memory
-                .min_block_size
-                .iter()
-                .map(|&x| log2_strict_usize(x as usize) as u8)
-                .collect(),
+            constant_trace_heights,
+            continuations_enabled,
+            as_alignment,
             self.config.system().memory_config.memory_dimensions(),
-        );
+            air_names,
+            widths.to_vec(),
+            interactions.to_vec(),
+        )
+        .with_max_trace_height(
+            self.config
+                .system()
+                .segmentation_strategy
+                .max_trace_height() as u32,
+        )
+        .with_max_cells(self.config.system().segmentation_strategy.max_cells());
 
         let mut exec_state = VmSegmentState::new(
             0,
@@ -741,13 +759,14 @@ where
         check_termination(exec_state.exit_code)?;
 
         // Check segment count
+        let segments = exec_state.ctx.into_segments();
         assert_eq!(
-            exec_state.ctx.segments.len(),
+            segments.len(),
             1,
             "Expected exactly 1 segment, but got {}",
-            exec_state.ctx.segments.len()
+            segments.len()
         );
-        let segment = exec_state.ctx.segments.into_iter().next().unwrap();
+        let segment = segments.into_iter().next().unwrap();
         Ok(segment.trace_heights)
     }
 
@@ -766,19 +785,19 @@ where
         )
         .unwrap();
 
-        let ctrl = TracegenExecutionControl::default();
         let mut segment = VmSegmentExecutor::new(
             chip_complex,
             self.trace_height_constraints.clone(),
             exe.fn_bounds.clone(),
-            ctrl,
+            TracegenExecutionControl,
         );
 
         if let Some(overridden_heights) = self.overridden_heights.as_ref() {
             segment.set_override_trace_heights(overridden_heights.clone());
         }
 
-        let mut exec_state = VmSegmentState::new(0, exe.pc_start, None, input.into(), rng, ());
+        let ctx = TracegenCtx::default();
+        let mut exec_state = VmSegmentState::new(0, exe.pc_start, None, input.into(), rng, ctx);
         segment.execute_spanned("execute_e3", &mut exec_state)?;
         Ok(segment)
     }

crates/vm/tests/integration_test.rs
@@ -8,7 +8,10 @@ use std::{
 use openvm_circuit::{
     arch::{
         create_and_initialize_chip_complex,
-        execution_mode::{e1::E1ExecutionControl, tracegen::TracegenExecutionControl},
+        execution_mode::{
+            e1::E1ExecutionControl,
+            tracegen::{TracegenCtx, TracegenExecutionControl},
+        },
         hasher::{poseidon2::vm_poseidon2_hasher, Hasher},
         interpreter::InterpretedInstance,
         ChipId, SingleSegmentVmExecutor, SystemTraceHeights, VirtualMachine, VmComplexTraceHeights,
@@ -779,16 +782,16 @@ fn test_hint_load_1() {
     )
     .unwrap();
 
-    let ctrl = TracegenExecutionControl::new(Some(segment.num_insns));
-    let mut segment = VmSegmentExecutor::<F, NativeConfig, _>::new(
+    let mut executor = VmSegmentExecutor::<F, NativeConfig, _>::new(
         chip_complex,
         vec![],
         Default::default(),
-        ctrl,
+        TracegenExecutionControl,
     );
 
-    let mut exec_state = VmSegmentState::new(0, 0, None, input.into(), rng, ());
-    segment.execute_from_state(&mut exec_state).unwrap();
+    let ctx = TracegenCtx::new(Some(segment.num_insns));
+    let mut exec_state = VmSegmentState::new(0, 0, None, input.into(), rng, ctx);
+    executor.execute_from_state(&mut exec_state).unwrap();
 
     let streams = exec_state.streams;
     assert!(streams.input_stream.is_empty());
@@ -844,19 +847,19 @@ fn test_hint_load_2() {
     )
     .unwrap();
 
-    let ctrl = TracegenExecutionControl::new(Some(segment.num_insns));
-    let mut segment = VmSegmentExecutor::<F, NativeConfig, _>::new(
+    let mut executor = VmSegmentExecutor::<F, NativeConfig, _>::new(
         chip_complex,
         vec![],
         Default::default(),
-        ctrl,
+        TracegenExecutionControl,
     );
 
-    let mut exec_state = VmSegmentState::new(0, 0, None, input.into(), rng, ());
-    segment.execute_from_state(&mut exec_state).unwrap();
+    let ctx = TracegenCtx::new(Some(segment.num_insns));
+    let mut exec_state = VmSegmentState::new(0, 0, None, input.into(), rng, ctx);
+    executor.execute_from_state(&mut exec_state).unwrap();
 
     let [read] = unsafe {
-        segment
+        executor
             .chip_complex
             .memory_controller()
             .memory
@@ -916,7 +919,7 @@ fn test_vm_pure_execution_non_continuation() {
 
     let executor = InterpretedInstance::<F, _>::new(test_native_config(), program);
     executor
-        .execute(E1ExecutionControl::new(None), vec![])
+        .execute(E1ExecutionControl, vec![])
         .expect("Failed to execute");
 }
 
@@ -943,6 +946,6 @@ fn test_vm_pure_execution_continuation() {
     let program = Program::from_instructions(&instructions);
     let executor = InterpretedInstance::<F, _>::new(test_native_continuations_config(), program);
     executor
-        .execute(E1ExecutionControl::new(None), vec![])
+        .execute(E1ExecutionControl, vec![])
         .expect("Failed to execute");
 }

crates/vm/src/arch/execution_mode/e1.rs
@@ -6,17 +6,18 @@ use crate::arch::{
     InsExecutorE1, VmChipComplex, VmConfig, VmSegmentState, VmStateMut,
 };
 
-pub type E1Ctx = ();
+#[derive(Default, derive_new::new)]
+pub struct E1Ctx {
+    pub instret_end: Option<u64>,
+}
 
 impl E1E2ExecutionCtx for E1Ctx {
     fn on_memory_operation(&mut self, _address_space: u32, _ptr: u32, _size: u32) {}
 }
 
 /// Implementation of the ExecutionControl trait using the old segmentation strategy
-#[derive(Default, derive_new::new)]
-pub struct E1ExecutionControl {
-    pub instret_end: Option<u64>,
-}
+#[derive(Default)]
+pub struct E1ExecutionControl;
 
 impl<F, VC> ExecutionControl<F, VC> for E1ExecutionControl
 where
@@ -26,14 +27,18 @@ where
 {
     type Ctx = E1Ctx;
 
-    fn initialize_context(&self) -> Self::Ctx {}
+    fn initialize_context(&self) -> Self::Ctx {
+        E1Ctx { instret_end: None }
+    }
 
     fn should_suspend(
         &self,
         state: &mut VmSegmentState<F, Self::Ctx>,
         _chip_complex: &VmChipComplex<F, VC::Executor, VC::Periphery>,
     ) -> bool {
-        self.instret_end
+        state
+            .ctx
+            .instret_end
             .is_some_and(|instret_end| state.instret >= instret_end)
     }
 

crates/vm/src/arch/execution_mode/metered/ctx.rs
@@ -1,163 +1,104 @@
 use openvm_instructions::riscv::RV32_IMM_AS;
-use serde::{Deserialize, Serialize};
 
-use crate::{
-    arch::{execution_mode::E1E2ExecutionCtx, PUBLIC_VALUES_AIR_ID},
-    system::memory::{dimensions::MemoryDimensions, CHUNK},
+use super::{
+    memory_ctx::MemoryCtx,
+    segment_ctx::{Segment, SegmentationCtx},
 };
-
-#[derive(Debug)]
-pub struct BitSet {
-    words: Box<[u64]>,
-}
-
-impl BitSet {
-    pub fn new(size_bits: usize) -> Self {
-        let num_words = 1 << size_bits.saturating_sub(6);
-        Self {
-            words: vec![0; num_words].into_boxed_slice(),
-        }
-    }
-
-    pub fn insert(&mut self, index: usize) -> bool {
-        let word_index = index / 64;
-        let bit_index = index % 64;
-        let mask = 1u64 << bit_index;
-
-        let was_set = (self.words[word_index] & mask) != 0;
-        self.words[word_index] |= mask;
-        !was_set
-    }
-
-    pub fn clear(&mut self) {
-        for item in self.words.iter_mut() {
-            *item = 0;
-        }
-    }
-}
+use crate::{arch::execution_mode::E1E2ExecutionCtx, system::memory::dimensions::MemoryDimensions};
 
 #[derive(Debug)]
 pub struct MeteredCtx<const PAGE_BITS: usize = 12> {
     pub trace_heights: Vec<u32>,
     pub is_trace_height_constant: Vec<bool>,
 
-    pub page_indices: BitSet,
-
-    pub instret_last_segment_check: u64,
-    pub segments: Vec<Segment>,
-
-    memory_dimensions: MemoryDimensions,
-    as_byte_alignment_bits: Vec<u8>,
-    boundary_idx: usize,
-    merkle_tree_index: Option<usize>,
-    adapter_offset: usize,
-    chunk: u32,
-    chunk_bits: u32,
+    pub memory_ctx: MemoryCtx<PAGE_BITS>,
+    pub segmentation_ctx: SegmentationCtx,
 }
 
 impl<const PAGE_BITS: usize> MeteredCtx<PAGE_BITS> {
     pub fn new(
-        num_traces: usize,
+        constant_trace_heights: Vec<Option<usize>>,
         continuations_enabled: bool,
         as_byte_alignment_bits: Vec<u8>,
         memory_dimensions: MemoryDimensions,
+        air_names: Vec<String>,
+        widths: Vec<usize>,
+        interactions: Vec<usize>,
     ) -> Self {
-        let boundary_idx = if continuations_enabled {
-            PUBLIC_VALUES_AIR_ID
-        } else {
-            PUBLIC_VALUES_AIR_ID + 1
-        };
+        let (trace_heights, is_trace_height_constant): (Vec<u32>, Vec<bool>) =
+            constant_trace_heights
+                .iter()
+                .map(|&constant_height| {
+                    if let Some(height) = constant_height {
+                        (height as u32, true)
+                    } else {
+                        (0, false)
+                    }
+                })
+                .unzip();
 
-        let merkle_tree_index = if continuations_enabled {
-            Some(boundary_idx + 1)
-        } else {
-            None
-        };
-
-        let adapter_offset = if continuations_enabled {
-            boundary_idx + 2
-        } else {
-            boundary_idx
-        };
-
-        let chunk = if continuations_enabled {
-            // Persistent memory uses CHUNK-sized blocks
-            CHUNK as u32
-        } else {
-            // Volatile memory uses single units
-            1
-        };
-
-        let chunk_bits = chunk.ilog2();
-        let merkle_height = memory_dimensions.overall_height();
+        let memory_ctx = MemoryCtx::new(
+            continuations_enabled,
+            as_byte_alignment_bits,
+            memory_dimensions,
+        );
+        let segmentation_ctx = SegmentationCtx::new(air_names, widths, interactions);
 
         Self {
-            trace_heights: vec![0; num_traces],
-            is_trace_height_constant: vec![false; num_traces],
-            page_indices: BitSet::new(merkle_height.saturating_sub(PAGE_BITS)),
-            instret_last_segment_check: 0,
-            segments: Vec::new(),
-            as_byte_alignment_bits,
-            boundary_idx,
-            merkle_tree_index,
-            adapter_offset,
-            chunk,
-            chunk_bits,
-            memory_dimensions,
+            trace_heights,
+            is_trace_height_constant,
+            memory_ctx,
+            segmentation_ctx,
         }
     }
 
-    fn update_boundary_merkle_heights(&mut self, address_space: u32, ptr: u32, size: u32) {
-        let num_blocks = (size + self.chunk - 1) >> self.chunk_bits;
-        let mut addr = ptr;
-        for _ in 0..num_blocks {
-            let block_id = addr >> self.chunk_bits;
-            let index = if self.chunk == 1 {
-                // Volatile
-                block_id
-            } else {
-                self.memory_dimensions
-                    .label_to_index((address_space, block_id)) as u32
-            } as usize;
+    pub fn with_max_trace_height(mut self, max_trace_height: u32) -> Self {
+        self.segmentation_ctx.set_max_trace_height(max_trace_height);
+        self
+    }
 
-            if self.page_indices.insert(index >> PAGE_BITS) {
-                // On page fault, assume we add all leaves in a page
-                let leaves = 1 << PAGE_BITS;
-                self.trace_heights[self.boundary_idx] += leaves;
+    pub fn with_max_cells(mut self, max_cells: usize) -> Self {
+        self.segmentation_ctx.set_max_cells(max_cells);
+        self
+    }
 
-                if let Some(merkle_tree_idx) = self.merkle_tree_index {
-                    let poseidon2_idx = self.trace_heights.len() - 2;
-                    self.trace_heights[poseidon2_idx] += leaves * 2;
+    pub fn with_max_interactions(mut self, max_interactions: usize) -> Self {
+        self.segmentation_ctx.set_max_interactions(max_interactions);
+        self
+    }
 
-                    let merkle_height = self.memory_dimensions.overall_height();
-                    let nodes = (((1 << PAGE_BITS) - 1) + (merkle_height - PAGE_BITS)) as u32;
-                    self.trace_heights[poseidon2_idx] += nodes * 2;
-                    self.trace_heights[merkle_tree_idx] += nodes * 2;
-                }
+    pub fn with_segment_check_insns(mut self, segment_check_insns: u64) -> Self {
+        self.segmentation_ctx
+            .set_segment_check_insns(segment_check_insns);
+        self
+    }
 
-                // At finalize, we'll need to read it in chunk-sized units for the merkle chip
-                self.update_adapter_heights_batch(address_space, self.chunk_bits, leaves);
+    pub fn segments(&self) -> &[Segment] {
+        &self.segmentation_ctx.segments
+    }
+
+    pub fn into_segments(self) -> Vec<Segment> {
+        self.segmentation_ctx.segments
+    }
+
+    fn reset_segment(&mut self) {
+        self.memory_ctx.clear();
+        for (i, &is_constant) in self.is_trace_height_constant.iter().enumerate() {
+            if !is_constant {
+                self.trace_heights[i] = 0;
             }
-
-            addr = addr.wrapping_add(self.chunk);
         }
     }
 
-    fn update_adapter_heights(&mut self, address_space: u32, size_bits: u32) {
-        self.update_adapter_heights_batch(address_space, size_bits, 1);
-    }
-
-    fn update_adapter_heights_batch(&mut self, address_space: u32, size_bits: u32, num: u32) {
-        let align_bits = self.as_byte_alignment_bits[address_space as usize];
-        debug_assert!(
-            align_bits as u32 <= size_bits,
-            "align_bits ({}) must be <= size_bits ({})",
-            align_bits,
-            size_bits
+    pub fn check_and_segment(&mut self, instret: u64) {
+        let did_segment = self.segmentation_ctx.check_and_segment(
+            instret,
+            &self.trace_heights,
+            &self.is_trace_height_constant,
         );
-        for adapter_bits in (align_bits as u32 + 1..=size_bits).rev() {
-            let adapter_idx = self.adapter_offset + adapter_bits as usize - 1;
-            self.trace_heights[adapter_idx] += num << (size_bits - adapter_bits + 1);
+
+        if did_segment {
+            self.reset_segment();
         }
     }
 }
@@ -176,18 +117,17 @@ impl<const PAGE_BITS: usize> E1E2ExecutionCtx for MeteredCtx<PAGE_BITS> {
 
         // Handle access adapter updates
         let size_bits = size.ilog2();
-        self.update_adapter_heights(address_space, size_bits);
+        self.memory_ctx
+            .update_adapter_heights(&mut self.trace_heights, address_space, size_bits);
 
         // Handle merkle tree updates
         // TODO(ayush): use a looser upper bound
         // see if this can be approximated by total number of reads/writes for AS != register
-        self.update_boundary_merkle_heights(address_space, ptr, size);
+        self.memory_ctx.update_boundary_merkle_heights(
+            &mut self.trace_heights,
+            address_space,
+            ptr,
+            size,
+        );
     }
-}
-
-#[derive(derive_new::new, Clone, Debug, Serialize, Deserialize)]
-pub struct Segment {
-    pub instret_start: u64,
-    pub num_insns: u64,
-    pub trace_heights: Vec<u32>,
 }

commit 24786c5bf10e136b46c3be6704ef6d7948a60582

fix(new-execution): no segmentation in single segment executor (#1796)
- fixes a regression introduced in #1789 where i accidentally deleted
the `.with_segment_check_insns(u64::MAX)` line
- added a test that `SingleSegmentVmExecutor` doesn't segment

crates/vm/src/arch/vm.rs
@@ -737,13 +737,7 @@ where
             widths.to_vec(),
             interactions.to_vec(),
         )
-        .with_max_trace_height(
-            self.config
-                .system()
-                .segmentation_strategy
-                .max_trace_height() as u32,
-        )
-        .with_max_cells(self.config.system().segmentation_strategy.max_cells());
+        .with_segment_check_insns(u64::MAX);
 
         let mut exec_state = VmSegmentState::new(
             0,

crates/vm/src/system/memory/persistent.rs
@@ -161,6 +161,13 @@ impl<F: PrimeField32, const CHUNK: usize> TouchedLabels<F, CHUNK> {
         }
     }
 
+    pub fn is_empty(&self) -> bool {
+        match self {
+            TouchedLabels::Running(touched_labels) => touched_labels.is_empty(),
+            TouchedLabels::Final(touched_labels) => touched_labels.is_empty(),
+        }
+    }
+
     pub fn len(&self) -> usize {
         match self {
             TouchedLabels::Running(touched_labels) => touched_labels.len(),

crates/vm/tests/integration_test.rs
@@ -14,8 +14,9 @@ use openvm_circuit::{
         },
         hasher::{poseidon2::vm_poseidon2_hasher, Hasher},
         interpreter::InterpretedInstance,
-        ChipId, SingleSegmentVmExecutor, SystemTraceHeights, VirtualMachine, VmComplexTraceHeights,
-        VmConfig, VmInventoryTraceHeights, VmSegmentExecutor, VmSegmentState,
+        ChipId, DefaultSegmentationStrategy, SingleSegmentVmExecutor, SystemTraceHeights,
+        VirtualMachine, VmComplexTraceHeights, VmConfig, VmInventoryTraceHeights,
+        VmSegmentExecutor, VmSegmentState,
     },
     system::{
         memory::{MemoryTraceHeights, VolatileMemoryTraceHeights, CHUNK},
@@ -948,4 +949,45 @@ fn test_vm_pure_execution_continuation() {
     executor
         .execute(E1ExecutionControl, vec![])
         .expect("Failed to execute");
+}
+
+#[test]
+fn test_single_segment_executor_no_segmentation() {
+    setup_tracing();
+    type F = BabyBear;
+
+    let mut config = test_native_config();
+
+    config.system.set_segmentation_strategy(Arc::new(
+        DefaultSegmentationStrategy::new_with_max_segment_len(1),
+    ));
+
+    let engine =
+        BabyBearPoseidon2Engine::new(standard_fri_params_with_100_bits_conjectured_security(3));
+    let vm = VirtualMachine::new(engine, config.clone());
+    let pk = vm.keygen();
+    let vk = pk.get_vk();
+    let instructions: Vec<_> = (0..1000)
+        .map(|_| Instruction::large_from_isize(ADD.global_opcode(), 0, 0, 1, 4, 0, 0, 0))
+        .chain(std::iter::once(Instruction::from_isize(
+            TERMINATE.global_opcode(),
+            0,
+            0,
+            0,
+            0,
+            0,
+        )))
+        .collect();
+
+    let program = Program::from_instructions(&instructions);
+    let single_vm = SingleSegmentVmExecutor::<F, _>::new(config);
+
+    let _ = single_vm
+        .execute_metered(
+            program.clone().into(),
+            vec![],
+            &vk.total_widths(),
+            &vk.num_interactions(),
+        )
+        .unwrap();
 }