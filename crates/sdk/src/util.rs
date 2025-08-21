11f854bca3721b6085d7b4e22463f955e11a64a1
Yi Sun <yi-sun@users.noreply.github.com>
2025-08-20 22:30:12 -0400

    chore: reduce warn to info for degree inequality (#2026)
    Closes INT-4825

--- crates/sdk/src/util.rs
+++ crates/sdk/src/util.rs
@@ -1,12 +1,27 @@
+use std::cmp::Ordering;
+
 use openvm_circuit::arch::SystemConfig;
 use openvm_stark_sdk::config::FriParameters;
 
 pub fn check_max_constraint_degrees(config: &SystemConfig, fri_params: &FriParameters) {
-    if config.max_constraint_degree != fri_params.max_constraint_degree() {
-        tracing::warn!(
-            "config.max_constraint_degree ({}) != fri_params.max_constraint_degree() ({})",
-            config.max_constraint_degree,
-            fri_params.max_constraint_degree()
-        );
+    match config
+        .max_constraint_degree
+        .cmp(&fri_params.max_constraint_degree())
+    {
+        Ordering::Greater => {
+            tracing::warn!(
+                "config.max_constraint_degree ({}) > fri_params.max_constraint_degree() ({})",
+                config.max_constraint_degree,
+                fri_params.max_constraint_degree()
+            );
+        }
+        Ordering::Less => {
+            tracing::info!(
+                "config.max_constraint_degree ({}) < fri_params.max_constraint_degree() ({})",
+                config.max_constraint_degree,
+                fri_params.max_constraint_degree()
+            );
+        }
+        Ordering::Equal => {}
     }
 }
