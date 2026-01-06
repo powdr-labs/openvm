e764f8017bafe9281fb71af6f148859f22d95914
qwang98 <qian.wang.wg24@wharton.upenn.edu>
2026-01-06 10:10:37 +0100

    bring back debug_assert for record_arena

--- crates/vm/src/arch/record_arena.rs
+++ crates/vm/src/arch/record_arena.rs
@@ -198,7 +198,7 @@ impl DenseRecordArena {
     /// Allocates `count` bytes and returns as a mutable slice.
     pub fn alloc_bytes<'a>(&mut self, count: usize) -> &'a mut [u8] {
         let begin = self.records_buffer.position();
-        assert!(
+        debug_assert!(
             begin as usize + count <= self.records_buffer.get_ref().len(),
             "failed to allocate {count} bytes from {begin} when the capacity is {}",
             self.records_buffer.get_ref().len()
